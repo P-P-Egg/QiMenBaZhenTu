d0da424f4e80169217285b201b7a4103e5729e13
mastermaye <550477197@qq.com>
2018-03-07 22:25:17 +0800

    坐标未完成

---
+++ Assets/sc/Shuxing.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuxing : MonoBehaviour {
+    public Vector2 zhuobiao;//坐标
+    public bool isdoor;
+
+		
+
+}
