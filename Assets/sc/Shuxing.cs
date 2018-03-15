7500d52da92501fef5e9d55252020c7b6b924f53
505856727 <505856727@qq.com>
2018-03-15 20:02:30 +0800

    jiaer

--- Assets/sc/Shuxing.cs
+++ Assets/sc/Shuxing.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shuxing : MonoBehaviour {
+    public bool ischaracter;
     public Vector2 zhuobiao;//坐标
     public bool isdoor;
-
-		
-
 }
+		
